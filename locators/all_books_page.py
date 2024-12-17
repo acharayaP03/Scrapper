cc04f7fb360eabe43c261d52547b032aa5859707
acharayaP03 <acharyaP03@gmail.com>
2024-12-17 12:32:41 +1100

    add locators for books page and book contents

---
+++ locators/all_books_page.py
@@ -0,0 +1,7 @@
+class AllBooksPageLocators:
+    """_summary_:
+    Locators for the books in the main
+    """
+    BOOKS = 'div.page_inner section li.col-xs-6.col-sm-4.col-md-3.col-lg-3'
+    PAGER = 'div.page_inner section ul.pager li.current'
+
